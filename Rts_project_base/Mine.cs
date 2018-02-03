17bcbcbbb36af5ff36223e86ece597503c410b85
Zeech <Morten_hansen51@yahoo.dk>
2018-02-03 19:57:35 +0100

    gre

--- Rts_project_base/Mine.cs
+++ Rts_project_base/Mine.cs
@@ -8,8 +8,30 @@ namespace Rts_project_base
 {
     class Mine : GameObject
     {
-        //key 
+        #region Fields
+        private int coalAmount;
+        private int goldAmount;  
+        
+
+        private int
+
+        #endregion
+        #region Constructor
+        public Mine()
+        {
+        }
+        #endregion;
+
+        #region Properties
+        public int CoalAmount { get => coalAmount; set => coalAmount = value; }
+
+        #endregion
 
 
+        #region Methods
+
+
+
+        #endregion
     }
 }
