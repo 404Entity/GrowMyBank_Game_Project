143e8244d6823918e4f3c27676a250045274a683
Johannes Andersen <defe@live.dk>
2018-02-03 16:36:07 +0100

    The basis of mine i think if you want to change go for it

--- Rts_project_base/Mine.cs
+++ Rts_project_base/Mine.cs
@@ -8,8 +8,31 @@ namespace Rts_project_base
 {
     class Mine : GameObject
     {
-        //key 
+        private bool busy = false;
+        private string name = "Mine";
+        private string Image = "";
 
+
+
+
+        //key 
+        public void Busy(bool busy)
+        {
+            if(busy == true)
+            {
+
+            }
+            else
+            {
+
+            }
+        }
+
+        public Mine(string name, bool busy)
+        {
+            this.busy = busy;
+            this.name = name;
+        }
 
     }
 }
